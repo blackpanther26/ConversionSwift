2a93f7a6a3ffeb72a171a0eab66fd0ceb8839425
blackpanther26 <priyanshuchahal1@gmail.com>
2024-07-29 22:05:21 +0530

    Initial Commit

---
+++ Conversion/ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionApp.swift
+//  Conversion
+//
+//  Created by Priyanshu Chahal on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
